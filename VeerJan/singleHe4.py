74b625b290aee5a4e0b06a5d13a19b60ca8b3ac2
Veer Kholia <veerkholia@outlook.com>
2026-03-03 16:15:53 +0000

    Current state of calculations (not with our system's parameters)

--- VeerJan/singleHe4.py
+++ VeerJan/singleHe4.py
@@ -1,16 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time = 5 #hours
+time = 4*3600 #hours
 pressure = 80 #bar
+temp_init = 3
+temp_cool = 0.843
+temp_step = 0.001
 
-tube_len = 10 #cm
-tube_wall = 0.25 #mm
-condenser_len = 1 #cm
-latent_heat = 0#
+tube_len = 10e-2
+tube_wall = 1e-3
+tube_dia = 0.6e-2
+condenser_len = 1e-2
+
+latent_heat = 85 #J mol^-1
 
 def thermal_conductivity(temperature):
-    #Return thermal conductivity of stainless steel at a given temperature
+    #Return thermal conductivity (W m^-1 K^-1) of stainless steel at a given temperature
     #Coefficients from NIST data for thermal conductivity: https://trc.nist.gov/cryogenics/materials/304Stainless/304Stainless_rev.htm
 
     coeffs = []
@@ -31,4 +36,30 @@ def thermal_conductivity(temperature):
 
     return 10**y
 
-temp_range = np.linspace(0.3, 1)
+def integrate(y, dx):
+    (y[1:]+y[:-1])*dx/2
+
+#"""
+temperatures = np.linspace(temp_cool, temp_init, int((temp_init-temp_cool)/temp_step))
+
+cooling_power = np.pi*tube_dia*tube_wall/tube_len * np.trapezoid(thermal_conductivity(temperatures), temperatures)
+print(f"Cooling power:\t{cooling_power} W")
+
+evap_rate = cooling_power/latent_heat #Moles per sec
+print(f"Moles of Helium:\t{evap_rate*time} mol")
+
+#print(f"Litres of He at STP: {evap_rate*time*22.4}")
+print(f"Litres of He at STP: {evap_rate*time*8.31*300/101.325e3 * 1000}")
+
+"""
+temp_range = np.linspace(0, 300, 100)
+k_range = thermal_conductivity(temp_range)
+
+fig = plt.figure()
+ax  =fig.add_subplot(111)
+ax.scatter(temp_range, k_range, marker='.')
+ax.set_xlabel("Temperature (K)")
+ax.set_ylabel("Thermal conductivity(W m^-1 K^-1)")
+plt.show()
+#"""
+
